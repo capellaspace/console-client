f9fbc35436ea53ebb0f49b7cd8f77c63eb49d6d0
Thomas Beyer <thomas.beyer@capellaspace.com>
2021-09-17 11:46:45 -0700

    improved exception handling and non explicit retryable errors

---
+++ tests/test_exception_handling.py
@@ -0,0 +1,43 @@
+from capella_console_client.exceptions import (
+    handle_error_response,
+    CollectionAccessDeniedError,
+    OrderExpiredError,
+    CapellaConsoleClientError,
+)
+
+import pytest
+from unittest.mock import MagicMock
+
+
+def create_mock_response(resp):
+    mock = MagicMock()
+    mock.json.return_value = resp
+    return mock
+
+
+@pytest.mark.parametrize(
+    "error_response,expected_error_class",
+    [
+        (
+            {"error": {"message": "Unknown error", "data": "NONONO"}},
+            CapellaConsoleClientError,
+        ),
+        (
+            {
+                "error": "Your organization is not permitted to access any of the collections your query is attempting to filter amongst (capella-mock)"
+            },
+            CollectionAccessDeniedError,
+        ),
+        ({"error": "order expired"}, OrderExpiredError),
+    ],
+)
+def test_handle_error_response(error_response, expected_error_class):
+    resp = create_mock_response(error_response)
+    with pytest.raises(expected_error_class) as excinfo:
+        handle_error_response(resp)
+
+    if isinstance(error_response["error"], str):
+        assert excinfo.value.message == error_response["error"]
+    else:
+        assert excinfo.value.message == error_response["error"]["message"]
+        assert excinfo.value.data == error_response["error"]["data"]
