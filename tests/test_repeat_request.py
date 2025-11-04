4835d1c530596f6eec63d126237cdf072efda1dd
Laxmi Balami <laxmi@live-eo.com>
2025-11-04 16:20:00 +0100

    Add test for valid and invalid contract id in repeat tasking

--- tests/test_repeat_request.py
+++ tests/test_repeat_request.py
@@ -6,7 +6,7 @@ import pytest
 
 from capella_console_client import CapellaConsoleClient
 from .test_data import post_mock_responses
-from capella_console_client.exceptions import RepeatRequestPayloadValidationError
+from capella_console_client.exceptions import RepeatRequestPayloadValidationError, ContractNotFoundError
 
 mock_geojson = {"coordinates": [-105.120360, 39.965330], "type": "Point"}
 
@@ -31,3 +31,13 @@ def test_create_repeat_request_end_and_count_defined(test_client):
         test_client.create_repeat_request(
             geometry=mock_geojson, name="test", repeat_end="PANDA", repetition_count=12345
         )
+
+
+def test_create_repeat_request_with_valid_contract_id(test_client, authed_repeat_request_mock):
+    repeat_request = test_client.create_repeat_request(geometry=mock_geojson, name="test", contract_id="contract-123")
+    assert repeat_request == post_mock_responses("/repeat-requests")
+
+
+def test_create_repeat_request_with_invalid_contract_id(test_client, authed_repeat_request_mock):
+    with pytest.raises(ContractNotFoundError):
+        test_client.create_repeat_request(geometry=mock_geojson, name="test", contract_id="invalid-contract")
